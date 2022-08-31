55523a15ca88647df00578a731f2dda7fefa2a90
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-08-31 17:26:14 +0200

    Rows, Columns & Basic Sizing in Compose

--- app/src/main/java/com/seif/learningcompose/MainActivity.kt
+++ app/src/main/java/com/seif/learningcompose/MainActivity.kt
@@ -3,16 +3,33 @@ package com.seif.learningcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.seif.learningcompose.ui.theme.LearningComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent { // has all the composables that it should draw
-            Greeting(name = "Seif Mohamed")
+            Column(
+                modifier = Modifier
+                    .background(Color.White)
+                    .fillMaxWidth()
+                    .fillMaxHeight(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = "Hello")
+                Text(text = "Seif")
+                Text(text = "Mohamed")
+            }
         }
     }
 }
@@ -25,5 +42,5 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() { // we can preview our composable
-        Greeting("Engineer Seif")
+    Greeting("Engineer Seif")
 }

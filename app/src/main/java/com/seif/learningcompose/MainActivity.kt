68bd23874916f789fff2626440fda8365703c30e
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-08-31 16:34:32 +0200

    Hello World with Compose

---
+++ app/src/main/java/com/seif/learningcompose/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.seif.learningcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.seif.learningcompose.ui.theme.LearningComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { // has all the composables that it should draw
+            Greeting(name = "Seif Mohamed")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() { // we can preview our composable
+        Greeting("Engineer Seif")
+}

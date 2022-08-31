764a344b6363d0528f2a9da6551bdde56f6a8682
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-08-31 23:05:28 +0200

    Creating an Image Card Composable

--- app/src/main/java/com/seif/learningcompose/MainActivity.kt
+++ app/src/main/java/com/seif/learningcompose/MainActivity.kt
@@ -3,43 +3,83 @@ package com.seif.learningcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.blur
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import com.seif.learningcompose.ui.theme.LearningComposeTheme
+import androidx.compose.ui.unit.sp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent { // has all the composables that it should draw
-            Column(
-                modifier = Modifier
-                    .background(Color.Green)
-                    .padding(top = 40.dp)
-                    .width(200.dp)
-                    .height(300.dp),
-                //  horizontalAlignment = Alignment.CenterHorizontally,
-                //  verticalArrangement = Arrangement.Center
-            ) {
-                Text(
-                    text = "Hello", modifier =
-                    Modifier
-                        .border(5.dp, Color.Black)
-                        .offset(20.dp, 20.dp)
-                        .border(5.dp, Color.White)
-                )
-                Spacer(modifier = Modifier.height(50.dp)) // will insert an empty composable that has hieght of 50dp
-                Text(text = "Seif")
-            }
+            val painters = painterResource(id = R.drawable.nyc_in_snow)
+            val description = "New York City in snow"
+            val title = "New York City in snow Image"
+            ImageCard(painter = painters, contentDescription = description, title = title)
         }
+    }
+}
+
+@Composable
+fun ImageCard(
+    painter: Painter, // allow us to use image from our image resource and display that in our image card
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+            .width(200.dp)
+            .height(220.dp)
+            .padding(20.dp),
+        shape = RoundedCornerShape(14.dp),
+        elevation = 6.dp
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxHeight()
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 300f
+                        )
+                    )
+            )
+            Text(
+                text = title,
+                color = Color.White,
+                fontSize = 16.sp,
+                modifier = Modifier
+                    .align(Alignment.BottomStart)
+                    .padding(12.dp)
+            )
+        } // a container that we can put composables into that and just apply a modifier(each item we added inside that box will be stacked on top of each other and can align them as well)
+
     }
 }
 
@@ -51,7 +91,11 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() { // we can preview our composable
-    Greeting("Engineer Seif")
+    val painters = painterResource(id = R.drawable.nyc_in_snow)
+    val description = "New York City in snow"
+    val title = "New York City in snow Image"
+    ImageCard(painter = painters, contentDescription = description, title = title)
+
 }
 
 // there is no margin in compose as we can do all the work with padding

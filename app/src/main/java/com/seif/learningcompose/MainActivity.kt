27896086ef66be192ae28eaf0afb099be04630f6
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-09-01 11:20:37 +0200

    Styling Text in Compose

--- app/src/main/java/com/seif/learningcompose/MainActivity.kt
+++ app/src/main/java/com/seif/learningcompose/MainActivity.kt
@@ -9,16 +9,29 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.fontResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -26,11 +39,52 @@ import androidx.compose.ui.unit.sp
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val fontFamily = FontFamily(
+            Font(R.font.lexend_thin, FontWeight.Thin),
+            Font(R.font.lexend_light, FontWeight.Light),
+            Font(R.font.lexend_extra_light, FontWeight.ExtraLight),
+            Font(R.font.lexend_black, FontWeight.Black),
+            Font(R.font.lexend_bold, FontWeight.Bold),
+            Font(R.font.lexend_semi_bold, FontWeight.SemiBold),
+            Font(R.font.lexend_extra_bold, FontWeight.ExtraBold),
+            Font(R.font.lexend_medium, FontWeight.Medium),
+            Font(R.font.lexend_regular, FontWeight.Normal),
+        )
         setContent { // has all the composables that it should draw
-            val painters = painterResource(id = R.drawable.nyc_in_snow)
-            val description = "New York City in snow"
-            val title = "New York City in snow Image"
-            ImageCard(painter = painters, contentDescription = description, title = title)
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0xFF101010))
+            ) {
+                Text(
+                    text = buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                            )
+                        ) {
+                            append('J')
+                        }
+                        append("etpack ")
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                            )
+                        ) {
+                            append('C')
+                        }
+                        append("ompose")
+                    },
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    fontFamily = fontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontStyle = FontStyle.Italic,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.align(Alignment.TopCenter),
+                    textDecoration = TextDecoration.Underline
+                )
+            }
         }
     }
 }
